be5206cfc9881428f5203f37bc78b10e9cbc6625
Rodion Ponomarenko <radion91@gmail.com>
2017-10-18 12:40:43 +0300

    add reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({
+import UserReducer from './reducer_user';
+import GuitarReducer from './reducer_guitar';
 
+const rootReducer = combineReducers({
+    user:UserReducer,
+    guitar:GuitarReducer
 });
 
 export default rootReducer;
