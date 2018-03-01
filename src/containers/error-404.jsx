58fbcbf614e69e89058ce99b756b2b7a9df9544b
Rodion P <radion91@gmail.com>
2018-03-01 19:44:30 +0200

    remove old project

--- src/containers/error-404.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NoMatch({ location }){
-    return (
-        <div style={{textAlign:'center'}}>
-            <h2>No match for <code>{location.pathname}</code></h2>
-        </div>
-    )
-}
